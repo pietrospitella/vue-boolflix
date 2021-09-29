7c30af8aff2ccd23c13e3dc6f1452b101845ae16
Pietro <pietrospitella@gmail.com>
2021-09-30 01:32:21 +0200

    milestone 3

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,12 @@
 <template>
   <li>
+    <img :src="`https://image.tmdb.org/t/p/w92${item.poster_path}`" alt="">
     <div class="item_title">{{item.title ? item.title : item.name}}</div>
     <div class="item_real_name">({{item.original_title ? item.original_title : item. original_name}})</div>
     <div :class="'flag-icon flag-icon-'+ (item.original_language == 'en' ? 'us': ' not_here' )"></div>
-    <div class="item_vote">{{item.vote_average}}</div>
+    <div class="item_vote">
+      <i v-for="n in 5" :key="n" class="fa-star" :class="(n <= starVote()) ? 'fas' : 'far'"></i>
+    </div>
   </li>
 </template>
 
@@ -20,7 +23,9 @@ export default {
       "item": Object
     },
     methods: {
-      
+      starVote(){
+        return Math.ceil(this.item.vote_average / 2);
+      }
     }
 }
 </script>
